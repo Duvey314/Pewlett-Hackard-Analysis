9a21cae61618910bc6a7d631556033c3716df702
Duvey <rudow.david@gmail.com>
2020-07-14 17:42:19 -0500

    updated employee databases

--- queries.sql
+++ queries.sql
@@ -10,3 +10,35 @@ SELECT COUNT(first_name)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--create sales retirement info
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	d.dept_name
+INTO retirement_info_sales
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+    AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01')
+	AND (dept_name = 'Sales');
+
+--create sales and developement retirement info
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	d.dept_name
+--INTO retirement_info_sales_dev
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+    AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01')
+	AND (dept_name IN ('Sales', 'Development'));
